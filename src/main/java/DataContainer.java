039a6e58cb5203ad8dbc8829b1d81c45f59050bd
SvetiVos <svetavostokova766@gmail.com>
2024-06-22 12:50:03 +0300

    Задачи из дз

---
+++ src/main/java/DataContainer.java
@@ -0,0 +1,12 @@
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class DataContainer {
+    int intValue;
+    String stringValue;
+    boolean booleanValue;
+}
---
+++ src/main/java/Task1.java
@@ -0,0 +1,8 @@
+import java.lang.annotation.*;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Task1 {
+    int days();
+}
